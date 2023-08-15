b50b397c4aef7c890bd3317251316595bbe96f2e
mohib <itsmohibuddin@gmail.com>
2023-08-15 07:48:32 +0530

    adding conditional tutorial file

---
+++ JS/13operators.js
@@ -0,0 +1,77 @@
+/*
+-> IN 3+5,
+-> '+' is operator & 3,5 are operand
+
+// Unary Operator
+// Binary Operator
+// Arithmatic Operators in JavaScript
+*/
+
+// Unary Operator
+x = 5;
+x = -x;
+console.log(x);
+
+//Binary Operator
+a = 34;
+b = "harry";
+c = a + b;
+console.log(c);
+
+//Arithmatic Operators in JavaScript
+var num1 = 5;
+var num2 = 8;
+console.log("The value of num1 + num2 is:", num1 + num2);
+console.log("The value of num1 - num2 is: " + (num1 - num2));
+console.log("The value of num1 * num2 is: " + num1 * num2);
+console.log("The value of num1 / num2 is: " + num1 / num2);
+console.log("The value of num1 % num2 is: " + num1 % num2);
+console.log("The value of num1 ** num2 is: " + num1 ** num2);
+
+console.log("The value of num1++ is: " + num1++);
+console.log("The value of ++num1 is: " + ++num1);
+
+console.log("The value of num2-- is: " + num2--);
+console.log("The value of --num2 is: " + --num2);
+
+console.log("the value is", num1);
+
+/*
+Assignment Operator
+=
++=
+-=
+*=
+/=
+%=
+**=
+
+String Operator
+Concatination Operator: +
+str + str2
+
+Comaprison Operator
+==
+===
+!=
+!==
+>
+<
+<=
+>=
+?
+
+Logical Operator
+&&
+||
+!
+
+Ternary Operator
+variable = (condition) ? true Statement : false statement
+
+typeOf Operator:- return variable type
+instanceOf Operator: return true or false
+
+Bitwise operator
+its work on 32 bit number
+*/
