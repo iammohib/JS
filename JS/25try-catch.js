b266deb44bd14a2761f0f1acb3337763ca01d459
mohib <itsmohibuddin@gmail.com>
2023-08-26 07:49:47 +0530

    adding tutorial of try-catch

---
+++ JS/25try-catch.js
@@ -0,0 +1,21 @@
+console.log("let's learn try-catch");
+console.log(`hello mohib`);
+
+const funct1 = (p) => {
+  console.log(`I am`,p);
+};
+
+console.log(funct1("aman"));
+
+const alertf = (params) => {
+    alert("hey")
+}
+try {
+    console.log("hey")
+    console.log(alertff());
+    console.log("done")
+} catch (error) {
+  console.log(error);
+}
+
+console.log("bye mohib")
